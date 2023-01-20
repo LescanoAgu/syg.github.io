5bca83fd9453ecbcb6b9e02f6bcd994702c19189
LescanoAgu <agulescano11@gmail.com>
2023-01-20 15:55:59 -0300

    PhPMailer
    Funcionando pero c/ errores

---
+++ phpmailer.php
@@ -0,0 +1,30 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] != 'POST'){
+header("Location: index.html");
+}
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+if( empty(trim($name)) ) $name = 'anonimo';
+
+$body = <<< HTML
+<p>De: $name / $email </p>
+<p>$message</p>
+HTML;
+
+$mailer = new PHPMailer();
+$mailer ->setFrom($email, $name);
+$mailer ->addAddress('consultas@sygingenieria.com' , 'Sitio Web');
+$mailer ->Subject = "Mensaje Web - $subject ";
+$mailer ->msgHTML($body);
+$mailer ->AltBody = strip_tags($body);
+$rta = $mailer -> send();
+var_dump($mailer);
